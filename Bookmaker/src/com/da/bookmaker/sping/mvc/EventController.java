073b4efb75a2b6932ec53850f3b1698163e58474
Dmitry <Dmitry@192.168.10.107>
2017-10-18 17:04:11 +0300

    Add controller for allEvents page.

---
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/EventController.java
@@ -0,0 +1,20 @@
+package com.da.bookmaker.sping.mvc;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.da.bookmaker.dao.DaoException;
+
+@Controller
+public class EventController extends BookmakerController{
+
+	@RequestMapping("/EventsList.spr")
+	public ModelAndView getMainList() throws DaoException {
+		Map<String, Object> map = null; 
+		map.putAll(getBookmakerList());
+		return new ModelAndView("allEvents", map);
+	}
+}
