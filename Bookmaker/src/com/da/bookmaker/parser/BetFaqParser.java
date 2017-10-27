003b2efa960e71fffc51473737d4377da0618d71
Dmitry <Dmitry@192.168.9.223>
2017-10-27 15:35:29 +0300

    betfaq parser fixes, 404 page

--- Bookmaker/src/com/da/bookmaker/parser/BetFaqParser.java
+++ Bookmaker/src/com/da/bookmaker/parser/BetFaqParser.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.w3c.dom.Node;
+
 import com.da.bookmaker.bean.IventBean;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
@@ -15,7 +17,10 @@ public class BetFaqParser {
 	static final private String URL = "https://betfaq.ru";
 
 	public static void main(String[] args) throws Exception {
-
+		new BetFaqParser().parse();
+	}
+	
+	public void parse() throws Exception {
 		WebClient webClient = new WebClient(BrowserVersion.CHROME);
 		try {
 
@@ -23,33 +28,66 @@ public class BetFaqParser {
 			HtmlPage page = (HtmlPage) webClient.getPage(URL);
 			List<IventBean> beans = new ArrayList<>();
 
+			
 			for (DomElement sport : page.getElementById("sticky").getChildElements()) {
-				if (sport.getAttribute("class").equals("soccer")) {
+				if (!sport.getAttribute("class").equals("soccer")) {
+					continue;
+				}
+				IventBean bean = new IventBean();
 					Iterator<DomElement> soccerChildren = sport.getChildElements().iterator();
 					soccerChildren.next(); // follow-wrapper
 					DomElement foreBets = soccerChildren.next(); // fore-bets
 					
                     DomElement tbody = foreBets.getFirstElementChild();
 					for (DomElement tr : tbody.getChildElements()) {
-						if (!tr.getAttribute("class").equals("statistics") && !tr.getAttribute("class").equals("head") && 
-								!tr.getAttribute("class").equals("premium") && !tr.getAttribute("class").equals("locked-vip")
-								&& !tr.getAttribute("class").equals("foot")){
+						if (!tr.getAttribute("class").trim().equals("statistics") && 
+								!tr.getAttribute("class").trim().equals("head") && 
+								!tr.getAttribute("class").trim().equals("premium") && 
+								!tr.getAttribute("class").trim().equals("locked-vip") && 
+								!tr.getAttribute("class").trim().equals("foot")){
 							Iterator<DomElement> tdChild = tr.getChildElements().iterator();
-							DomElement small = tdChild.next();
-							for(DomElement a : small.getChildElements()){
-								String country = a.getTextContent();
-								System.out.println(country);
-							}
-							
+							tdChild.next(); // flag
+							DomElement title = tdChild.next(); // title
+							String competition = getCompetision(title);
+							String name = getName(title);
+							double coefficient = getCoefficient(tdChild.next());
+							System.out.println(name + " " + coefficient + " " + competition);
 						}
-
 					}
-
-				}
 			}
 		} finally {
 			webClient.closeAllWindows();
 		}
 	}
-
+	
+	private double getCoefficient(DomElement coeff){
+		Iterator<DomElement> iterator = coeff.getChildElements().iterator();
+		DomElement tagA = iterator.next();
+		String coefficient = tagA.getTextContent().trim(); // нужен каст в int
+		return Double.parseDouble(coefficient);
+	}
+	
+	private String getName(DomElement title){
+		Iterator<DomElement> titleChild = title.getChildElements().iterator();
+		titleChild.next();
+		DomElement team = titleChild.next(); // teams
+		Iterator<DomElement> teamChild = team.getChildElements().iterator();
+		teamChild.next(); // time
+		DomElement fore = teamChild.next(); //fore
+		Node element = fore.getLastChild();
+		return element.getTextContent();
+	}
+	
+	private String getCompetision(DomElement title){
+		Iterator<DomElement> titleChild = title.getChildElements().iterator();
+		DomElement small = titleChild.next();
+		String competition = "";
+		for (DomElement a : small.getChildElements()){
+			if (competition.length() != 0){
+				competition += "/";
+			}
+			competition += a.getTextContent();
+		}
+		return competition;
+	}
 }
