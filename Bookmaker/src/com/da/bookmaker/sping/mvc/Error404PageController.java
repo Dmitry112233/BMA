efb9d4c46519fc36d737c08e8ba8d3f8301e5d1e
Dmitry <Dmitry@192.168.10.101>
2017-11-03 13:30:25 +0300

    add controller for 404 page.

---
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/Error404PageController.java
@@ -0,0 +1,19 @@
+package com.da.bookmaker.sping.mvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.da.bookmaker.dao.DaoException;
+
+public class Error404PageController extends BookmakerController{
+	
+	@RequestMapping("/Error404Page.spr")
+	public ModelAndView getMainList() throws DaoException {
+		Map<String, Object> map = new HashMap<>();
+		map.putAll(getBookmakerList());
+		return new ModelAndView("404", map);
+	}
+}
