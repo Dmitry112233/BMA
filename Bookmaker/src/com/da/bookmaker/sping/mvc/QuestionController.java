6d9af59c731e9a7fb317df46b97b665b6d0273f2
Dmitry <Dmitry@DESKTOP-U7R7K8T>
2018-01-10 20:29:33 +0300

    Fixed link to counter controller on the question page.

--- Bookmaker/src/com/da/bookmaker/sping/mvc/QuestionController.java
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/QuestionController.java
@@ -31,7 +31,7 @@ public class QuestionController extends BookmakerController {
 			String str = bean.getAnswer();
 			BookmakerBean xBet = getBookmakerList().get("bookmakerList").get("1xBet");
 			if (str.contains("BKLINKBET")){
-			String replaceString = str.replace("BKLINKBET", xBet.getLink());
+			String replaceString = str.replace("BKLINKBET", "Counter.spr?id=${bookmaker.bookMakerId}");
 			System.out.println(replaceString);
 			bean.setAnswer(replaceString);
 			}else{
