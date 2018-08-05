312f1e55cada4ad5d3b7f59634003beae90ec277
Dmitry <Dmitry@192.168.9.9>
2018-08-05 19:06:56 +0300

    fix

--- BookmakerParser/src/com/da/bookmaker/parser/WildstatParser.java
+++ BookmakerParser/src/com/da/bookmaker/parser/WildstatParser.java
@@ -501,11 +501,12 @@ public class WildstatParser {
 		iterator.next();
 		DomElement td = iterator.next();
 		String team1 = td.getFirstElementChild().getTextContent().trim();
-		if (team1.contains("Арсенал") && url.equals(property.getProperty("RUS_CURRENT"))
+		if (team1.equals("Арсенал") || team1.equals("Арсенал (Тула)") && url.equals(property.getProperty("RUS_CURRENT"))
 				|| url.equals(property.getProperty("RUS_CUP_CURRENT")) || url.equals(property.getProperty("RUS_16_17"))
 				|| url.equals(property.getProperty("RUS_CUP_16_17"))) {
 			team1 = "Арсенал Тула";
 		}
+		System.out.println(team1);
 		return team1;
 	}
 
@@ -518,11 +519,12 @@ public class WildstatParser {
 		iterator.next();
 		DomElement td = iterator.next();
 		String team2 = td.getFirstElementChild().getTextContent().trim();
-		if (team2.contains("Арсенал") && url.equals(property.getProperty("RUS_CURRENT"))
+		if (team2.equals("Арсенал") || team2.equals("Арсенал (Тула)") && url.equals(property.getProperty("RUS_CURRENT"))
 				|| url.equals(property.getProperty("RUS_CUP_CURRENT")) || url.equals(property.getProperty("RUS_16_17"))
 				|| url.equals(property.getProperty("RUS_CUP_16_17"))) {
 			team2 = "Арсенал Тула";
 		}
+		System.out.println(team2);
 		return team2;
 	}
 
