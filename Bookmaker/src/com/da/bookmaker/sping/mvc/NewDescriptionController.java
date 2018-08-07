952ba45822fd35142d3686678bbcad7416cb028f
Dmitry <Dmitry@192.168.10.142>
2018-08-07 22:00:37 +0300

    Fixed

--- Bookmaker/src/com/da/bookmaker/sping/mvc/NewDescriptionController.java
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/NewDescriptionController.java
@@ -3,6 +3,7 @@ package com.da.bookmaker.sping.mvc;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,6 +12,7 @@ import com.da.bookmaker.bean.NewsBean;
 import com.da.bookmaker.dao.DaoException;
 import com.da.bookmaker.dao.DaoFactory;
 
+@Controller
 public class NewDescriptionController extends BookmakerController {
 
 	@RequestMapping("/NewsDescription_{id}_details.spr")
