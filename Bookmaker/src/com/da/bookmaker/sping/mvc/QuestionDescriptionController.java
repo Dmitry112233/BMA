6a2c2d2569d1989d1589fca8ce38299b3b1de642
Dmitry <Dmitry@172.21.2.147>
2018-09-12 16:34:56 +0300

    Add meta tags for other questions

--- Bookmaker/src/com/da/bookmaker/sping/mvc/QuestionDescriptionController.java
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/QuestionDescriptionController.java
@@ -32,23 +32,67 @@ public class QuestionDescriptionController extends BookmakerController {
 				fis = Thread.currentThread().getContextClassLoader().getResourceAsStream("copies.properties");
 				property.load(fis);
 			}
-			if (id == 1) {
+			switch (id) {
+			case 1:
 				map.put("Question_T", property.getProperty("Question1_T"));
 				map.put("Question_H1", property.getProperty("Question1_H1"));
 				map.put("Question_D", property.getProperty("Question1_D"));
 				map.put("Question_Txt", property.getProperty("Question1_Txt"));
-			}
-			if (id == 6) {
+				break;
+			case 2:
+				map.put("Question_T", property.getProperty("Question2_T"));
+				map.put("Question_H1", property.getProperty("Question2_H1"));
+				map.put("Question_D", property.getProperty("Question2_D"));
+				map.put("Question_Txt", property.getProperty("Question2_Txt"));
+				break;
+			case 3:
+				map.put("Question_T", property.getProperty("Question3_T"));
+				map.put("Question_H1", property.getProperty("Question3_H1"));
+				map.put("Question_D", property.getProperty("Question3_D"));
+				map.put("Question_Txt", property.getProperty("Question3_Txt"));
+				break;
+			case 4:
+				map.put("Question_T", property.getProperty("Question4_T"));
+				map.put("Question_H1", property.getProperty("Question4_H1"));
+				map.put("Question_D", property.getProperty("Question4_D"));
+				map.put("Question_Txt", property.getProperty("Question4_Txt"));
+				break;
+			case 5:
+				map.put("Question_T", property.getProperty("Question5_T"));
+				map.put("Question_H1", property.getProperty("Question5_H1"));
+				map.put("Question_D", property.getProperty("Question5_D"));
+				map.put("Question_Txt", property.getProperty("Question5_Txt"));
+				break;
+			case 6:
 				map.put("Question_T", property.getProperty("Question6_T"));
 				map.put("Question_H1", property.getProperty("Question6_H1"));
 				map.put("Question_D", property.getProperty("Question6_D"));
 				map.put("Question_Txt", property.getProperty("Question6_Txt"));
-			}
-			if (id == 10) {
+				break;
+			case 7:
+				map.put("Question_T", property.getProperty("Question7_T"));
+				map.put("Question_H1", property.getProperty("Question7_H1"));
+				map.put("Question_D", property.getProperty("Question7_D"));
+				map.put("Question_Txt", property.getProperty("Question7_Txt"));
+				break;
+			case 8:
+				map.put("Question_T", property.getProperty("Question8_T"));
+				map.put("Question_H1", property.getProperty("Question8_H1"));
+				map.put("Question_D", property.getProperty("Question8_D"));
+				map.put("Question_Txt", property.getProperty("Question8_Txt"));
+				break;
+			case 9:
+				map.put("Question_T", property.getProperty("Question9_T"));
+				map.put("Question_H1", property.getProperty("Question9_H1"));
+				map.put("Question_D", property.getProperty("Question9_D"));
+				map.put("Question_Txt", property.getProperty("Question9_Txt"));
+				break;
+			case 10:
 				map.put("Question_T", property.getProperty("Question10_T"));
 				map.put("Question_H1", property.getProperty("Question10_H1"));
 				map.put("Question_D", property.getProperty("Question10_D"));
 				map.put("Question_Txt", property.getProperty("Question10_Txt"));
+				break;
 			}
 		} catch (IOException e) {
 			System.err.println("Файл отсутствует");
