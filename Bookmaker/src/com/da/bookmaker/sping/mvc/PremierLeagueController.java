3d9306281890b0f4b44afe5a7de1da4378942a91
Dmitry <Dmitry@DESKTOP-U7R7K8T>
2018-09-23 19:01:46 +0300

    implement post for nav

--- Bookmaker/src/com/da/bookmaker/sping/mvc/PremierLeagueController.java
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/PremierLeagueController.java
@@ -2,6 +2,8 @@ package com.da.bookmaker.sping.mvc;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.text.ParseException;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.Properties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.da.bookmaker.bean.PremierLeagueBean;
@@ -22,6 +25,58 @@ public class PremierLeagueController extends BookmakerController {
 
 	private InputStream fis;
 	static private Properties property;
+	
+	@RequestMapping("/match_list")
+	public ModelAndView getMatchList(@RequestParam("league") String league) throws DaoException, ParseException, IOException{
+		league = new String(league.getBytes("iso-8859-1"), "UTF-8");
+		Map<String, Object> map = getMatchesList(league); 
+		System.out.println(league);
+		map.putAll(getBookmakerList());
+		try {
+			if (property == null) {
+				property = new Properties();
+				fis = Thread.currentThread().getContextClassLoader().getResourceAsStream("copies.properties");
+				property.load(fis);
+			}
+			if (league.equals("Английская Примьер Лига")){
+				map.put("League_T", property.get("ENG_League_T"));
+				map.put("League_H1", property.get("ENG_League_H1"));
+				map.put("League_D", property.get("ENG_League_D"));
+				map.put("League_Txt", property.get("ENG_League_Txt"));
+			}
+			if (league.equals("Испанская Ла Лига")){
+				map.put("League_T", property.get("SPA_League_T"));
+				map.put("League_H1", property.get("SPA_League_H1"));
+				map.put("League_D", property.get("SPA_League_D"));
+				map.put("League_Txt", property.get("SPA_League_Txt"));
+			}
+			if (league.equals("Российская Примьер Лига")){
+				map.put("League_T", property.get("RUS_League_T"));
+				map.put("League_H1", property.get("RUS_League_H1"));
+				map.put("League_D", property.get("RUS_League_D"));
+				map.put("League_Txt", property.get("RUS_League_Txt"));
+			}
+			if (league.equals("Немецкая Бундеслига")){
+				map.put("League_T", property.get("GER_League_T"));
+				map.put("League_H1", property.get("GER_League_H1"));
+				map.put("League_D", property.get("GER_League_D"));
+				map.put("League_Txt", property.get("GER_League_Txt"));
+			}
+			if (league.equals("Итальянская серия А")){
+				map.put("League_T", property.get("ITA_League_T"));
+				map.put("League_H1", property.get("ITA_League_H1"));
+				map.put("League_D", property.get("ITA_League_D"));
+				map.put("League_Txt", property.get("ITA_League_Txt"));
+			}
+		} catch (IOException e) {
+			System.err.println("Файл отсутствует");
+		} finally {
+			if (fis != null) {
+				fis.close();
+			}
+		}	
+		return new ModelAndView("league", map);
+	}
 
 	@RequestMapping("/PremierLeague_{leagueName}_List")
 	public ModelAndView getMainList(@PathVariable("leagueName") String leagueName) throws DaoException, ParseException, IOException {
