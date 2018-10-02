ab843ec19a3560880773ef99d5a2e042f2916cfc
Dmitry <Dmitry@DESKTOP-U7R7K8T>
2018-10-02 21:36:46 +0300

    Fixed league description controller. Add meta, also in table controller

--- Bookmaker/src/com/da/bookmaker/sping/mvc/CompetishionsDecriptionController.java
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/CompetishionsDecriptionController.java
@@ -21,24 +21,39 @@ public class CompetishionsDecriptionController extends BookmakerController {
 
 	@RequestMapping("/{league}_описание")
 	public ModelAndView getLeagueDescription(@PathVariable("league") String league) throws DaoException, IOException {
+		Map<String, Object> map = new HashMap<>();
 		try {
 			if (property == null) {
 				property = new Properties();
 				fis = Thread.currentThread().getContextClassLoader().getResourceAsStream("leagueDescription.properties");
 				property.load(fis);
 			}
-			Map<String, Object> map = new HashMap<>();
 			map.putAll(getBookmakerList());
 			switch (league){
 			case "Английская Примьер Лига" : map.put("league", property.getProperty("ENG"));
+			map.put("league_D", property.getProperty("ENG_D"));
+			map.put("league_H1", property.getProperty("ENG_H1"));
+			map.put("league_T", property.getProperty("ENG_T"));
 			break;
 			case "Испанская Ла Лига": map.put("league", property.getProperty("SPA"));
+			map.put("league_D", property.getProperty("SPA_D"));
+			map.put("league_H1", property.getProperty("SPA_H1"));
+			map.put("league_T", property.getProperty("SPA_T"));
 			break;
 			case "Российская Примьер Лига" : map.put("league", property.getProperty("RUS"));
+			map.put("league_D", property.getProperty("RUS_D"));
+			map.put("league_H1", property.getProperty("RUS_H1"));
+			map.put("league_T", property.getProperty("RUS_T"));
 			break;
 			case "Немецкая Бундеслига" : map.put("league", property.getProperty("GER"));
+			map.put("league_D", property.getProperty("GER_D"));
+			map.put("league_H1", property.getProperty("GER_H1"));
+			map.put("league_T", property.getProperty("GER_T"));
 			break;
 			case "Итальянская серия А" : map.put("league", property.getProperty("ITA"));
+			map.put("league_D", property.getProperty("ITA_D"));
+			map.put("league_H1", property.getProperty("ITA_H1"));
+			map.put("league_T", property.getProperty("ITA_T"));
 			break;
 			}
 		} catch (IOException e) {
@@ -48,6 +63,6 @@ public class CompetishionsDecriptionController extends BookmakerController {
 				fis.close();
 			}
 		}
-		return new ModelAndView("leagueDescriptionPage");
+		return new ModelAndView("leagueDescriptionPage", map);
 	}
 }
--- Bookmaker/src/com/da/bookmaker/sping/mvc/CompetishionsTableController.java
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/CompetishionsTableController.java
@@ -1,8 +1,11 @@
 package com.da.bookmaker.sping.mvc;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +18,61 @@ import com.da.bookmaker.dao.DaoFactory;
 
 @Controller
 public class CompetishionsTableController extends BookmakerController{
+	
+	private InputStream fis;
+	static private Properties property;
 
 	@RequestMapping("/{league}_table")
-	public ModelAndView getTable(@PathVariable("league") String league) throws DaoException{
+	public ModelAndView getTable(@PathVariable("league") String league) throws DaoException, IOException{
 		List<LeagueTableBean> table = DaoFactory.getLeaguTableDao().getTableForLeague(league);
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.putAll(getBookmakerList());
 		map.put("table", table);
+		try {
+			if (property == null) {
+				property = new Properties();
+				fis = Thread.currentThread().getContextClassLoader().getResourceAsStream("copies.properties");
+				property.load(fis);
+			}
+			switch (league) {
+			case "английская примьер лига":
+				map.put("Table_T", property.get("ENG_Table_T"));
+				map.put("Table_H1", property.get("ENG_Table_H1"));
+				map.put("Table_D", property.get("ENG_Table_D"));
+				map.put("Table_Txt", property.get("ENG_Table_Txt"));
+				break;
+			case "испанская ла лига":
+				map.put("Table_T", property.get("SPA_Table_T"));
+				map.put("Table_H1", property.get("SPA_Table_H1"));
+				map.put("Table_D", property.get("SPA_Table_D"));
+				map.put("Table_Txt", property.get("SPA_Table_Txt"));
+				break;
+			case "российская примьер лига":
+				map.put("Table_T", property.get("RUS_Table_T"));
+				map.put("Table_H1", property.get("RUS_Table_H1"));
+				map.put("Table_D", property.get("RUS_Table_D"));
+				map.put("Table_Txt", property.get("RUS_Table_Txt"));
+				break;
+			case "немецкая бундеслига":
+				map.put("Table_T", property.get("GER_Table_T"));
+				map.put("Table_H1", property.get("GER_Table_H1"));
+				map.put("Table_D", property.get("GER_Table_D"));
+				map.put("Table_Txt", property.get("GER_Table_Txt"));
+				break;
+			case "итальянская серия а":
+				map.put("Table_T", property.get("ITA_Table_T"));
+				map.put("Table_H1", property.get("ITA_Table_H1"));
+				map.put("Table_D", property.get("ITA_Table_D"));
+				map.put("Table_Txt", property.get("ITA_Table_Txt"));
+				break;
+			}
+		} catch (IOException e) {
+			System.err.println("Файл отсутствует");
+		} finally {
+			if (fis != null) {
+				fis.close();
+			}
+		}
 		return new ModelAndView("leagueTable", map);
 	}
 }
