8255d4ba7e438ee0f673cb667903f78419619864
dmitry.starovoytov <dmitry.starovoytov@starovoytovd.synesis.local>
2018-11-15 18:30:58 +0300

    rename all team to wildstat names on competiton top matches

--- Bookmaker/src/com/da/bookmaker/sping/mvc/CompetitionsController.java
+++ Bookmaker/src/com/da/bookmaker/sping/mvc/CompetitionsController.java
@@ -2,7 +2,10 @@ package com.da.bookmaker.sping.mvc;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.da.bookmaker.bean.PremierLeagueBean;
 import com.da.bookmaker.dao.DaoException;
 import com.da.bookmaker.dao.DaoFactory;
 
@@ -18,16 +22,16 @@ public class CompetitionsController extends BookmakerController {
 
 	private InputStream fis;
 	static private Properties property;
-	
+
 	@RequestMapping("/competitions")
 	public ModelAndView getMainList() throws DaoException, IOException {
 		Map<String, Object> map = new HashMap<>();
 		map.putAll(getBookmakerList());
-		map.put("eng_matches", DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Английская Премьер Лига"));
-		map.put("spa_matches", DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Испанская Ла Лига"));
-		map.put("ita_matches", DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Итальянская серия А"));
-		map.put("rus_matches", DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Российская Премьер Лига"));
-		map.put("ger_matches", DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Немецкая Бундеслига"));
+		map.put("eng_matches", setWildstatNames(DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Английская Премьер Лига")));
+		map.put("spa_matches", setWildstatNames(DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Испанская Ла Лига")));
+		map.put("ita_matches", setWildstatNames(DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Итальянская серия А")));
+		map.put("rus_matches", setWildstatNames(DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Российская Премьер Лига")));
+		map.put("ger_matches", setWildstatNames(DaoFactory.getPremierLeagueDao().getAllMatchesForLeague("Немецкая Бундеслига")));
 		try {
 			if (property == null) {
 				property = new Properties();
@@ -44,7 +48,15 @@ public class CompetitionsController extends BookmakerController {
 			if (fis != null) {
 				fis.close();
 			}
-		}	
+		}
 		return new ModelAndView("allCompetitions", map);
+	}
+
+	private List<PremierLeagueBean> setWildstatNames(List<PremierLeagueBean> list) throws DaoException {
+			for (PremierLeagueBean bean : list) {
+				bean.setTeam1(DaoFactory.getMatchDetailsDao().getWildstatNameFromDictionary(bean.getTeam1()));
+				bean.setTeam2(DaoFactory.getMatchDetailsDao().getWildstatNameFromDictionary(bean.getTeam2()));
+			}
+		return list;
 	}
 }
